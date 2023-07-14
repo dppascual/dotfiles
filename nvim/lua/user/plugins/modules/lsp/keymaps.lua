a0b5460cb09ce27911be74bde312e17069e95617
Daniel Peña <dppascual@gmail.com>
2023-07-14 20:04:56 +0200

    better lsp formatting

--- nvim/lua/user/plugins/modules/lsp/keymaps.lua
+++ nvim/lua/user/plugins/modules/lsp/keymaps.lua
@@ -115,41 +115,12 @@ M.on_attach = function(client, bufnr)
         vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = bufnr,
             callback = function()
-                if vim.api.nvim_buf_get_option(bufnr, 'filetype') == 'go' then
-                    local params = vim.lsp.util.make_range_params(
-                        nil,
-                        client.offset_encoding
-                    )
-                    params.context = { only = { 'source.organizeImports' } }
-
-                    local result = vim.lsp.buf_request_sync(
-                        bufnr,
-                        'textDocument/codeAction',
-                        params,
-                        2000
-                    )
-                    for _, res in pairs(result or {}) do
-                        for _, r in pairs(res.result or {}) do
-                            if r.edit then
-                                vim.lsp.util.apply_workspace_edit(
-                                    r.edit,
-                                    'utf-16'
-                                )
-                            else
-                                vim.lsp.buf.execute_command(r.command)
-                            end
-                        end
-                    end
-
-                    vim.lsp.buf.format()
-                else
-                    vim.lsp.buf.format({
-                        filter = function(cli)
-                            return cli.name ~= 'sumneko_lua'
-                        end,
-                        bufnr = bufnr,
-                    })
-                end
+                vim.lsp.buf.format({
+                    filter = function(cli)
+                        return cli.name ~= 'sumneko_lua'
+                    end,
+                    bufnr = bufnr,
+                })
             end,
             group = group,
         })
