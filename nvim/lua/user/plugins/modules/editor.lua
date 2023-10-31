a17e294aa31b0114eb05ee0d84c389493617940b
Daniel Peña <dppascual@gmail.com>
2023-10-31 11:45:01 +0100

    add new motions with flash.nvim

--- nvim/lua/user/plugins/modules/editor.lua
+++ nvim/lua/user/plugins/modules/editor.lua
@@ -274,6 +274,50 @@ return {
         end,
     },
 
+    -- Flash enhances the built-in search functionality by showing labels
+    -- at the end of each match, letting you quickly jump to a specific
+    -- location.
+    {
+        'folke/flash.nvim',
+        event = 'VeryLazy',
+        ---@type Flash.Config
+        opts = {
+            jump = { nohlsearch = true },
+            prompt = {
+                -- Place the prompt above the statusline.
+                win_config = { row = -2 },
+            },
+            search = {
+                exclude = {
+                    'cmp_menu',
+                    'flash_prompt',
+                    'qf',
+                    function(win)
+                        -- Floating windows from bqf.
+                        if
+                            vim.api
+                                .nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+                                :match('BqfPreview')
+                        then
+                            return true
+                        end
+
+                        -- Non-focusable windows.
+                        return not vim.api.nvim_win_get_config(win).focusable
+                    end,
+                },
+            },
+        },
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    },
+
     -- Harpoon
     --
     {
